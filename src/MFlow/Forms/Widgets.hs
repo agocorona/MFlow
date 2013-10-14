b9165aa7d46f731425f77b7563ed1c7d363d79b7
Alberto G. Corona <agocorona@gmail.com>
2013-10-14 18:18:09 +0200

    demo

--- src/MFlow/Forms/Widgets.hs
+++ src/MFlow/Forms/Widgets.hs
@@ -443,7 +443,7 @@ instance Indexable TField where
 instance Serializable TField where
     serialize= B.pack . show
     deserialize= read . B.unpack
-    setPersist = const $!  Just filePersist
+    setPersist _=  Just filePersist
 
 
 writetField k s= atomically $ writeDBRef (getDBRef k) $ TField k $ toByteString s
