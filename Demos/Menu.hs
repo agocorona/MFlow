2840a514e8d95e617ff94964f45b6d9f621f9744
Alberto G. Corona <agocorona@gmail.com>
2013-11-16 13:15:56 +0100

    demo

--- Demos/Menu.hs
+++ Demos/Menu.hs
@@ -24,6 +24,8 @@ import Data.TCache.Memoization
 import Language.Haskell.HsColour
 import Language.Haskell.HsColour.Colourise
 import Text.Hamlet
+import Text.Julius
+
 
 
 newtype Filename= Filename String deriving Typeable
@@ -361,7 +363,39 @@ wiki =  do
         El.title $ fromString pag)
         ++> ( h1 ! At.style "text-align:center" <<<  tFieldEd "editor" (wikip ++pag ++ "title.html") (fromString pag))
         **> tFieldEd "editor" (wikip ++ pag ++ "body.html") "Enter the body"
-        **> noWidget
+        <++ shareHtml
+        <>  script << shareScript
 
 wikip="wiki/"
 
+shareScript= renderJavascript [julius|
+(function(doc, script) {
+  var js,
+      fjs = doc.getElementsByTagName(script)[0],
+      frag = doc.createDocumentFragment(),
+      add = function(url, id) {
+          if (doc.getElementById(id)) {return;}
+          js = doc.createElement(script);
+          js.src = url;
+          id && (js.id = id);
+          frag.appendChild( js );
+      };
+
+    // Google+ button
+    add('http://apis.google.com/js/plusone.js');
+    // Facebook SDK
+    add('//connect.facebook.net/en_US/all.js#xfbml=1&appId=200103733347528', 'facebook-jssdk');
+    // Twitter SDK
+    add('//platform.twitter.com/widgets.js');
+
+    fjs.parentNode.insertBefore(frag, fjs);
+}(document, 'script'));
+|]
+
+shareHtml= [shamlet|<a href="https://twitter.com/share" class="twitter-share-button" data-count="horizontal">Tweet</a>
+
+<div class="g-plusone" data-size="medium" data-count="true"></div>
+
+<div id="fb-root"></div>
+<div class="fb-like" data-send="false" data-layout="button_count" data-width="1" data-show-faces="false" data-action="recommend"></div>
+|]
