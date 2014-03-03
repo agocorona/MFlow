7c6833acd891bc75d860ef9e6cf408d07a228027
Alberto G. Corona <agocorona@gmail.com>
2014-03-03 23:02:17 +0100

    demo

--- Demos/Menu.hs
+++ Demos/Menu.hs
@@ -414,8 +414,6 @@ wiki =  page $ do
         **>   tFieldEd "editor" (wikip ++ pagname ++ "body.html") "Enter the body"
         <++ do
            hr
---           shareHtml
---           shareScript
            disquscript
 
         )
@@ -444,36 +442,33 @@ disquscript= [shamlet|
 
 wikientries= return . filter  (isPrefixOf wikip . fst)  =<< indexOf tfieldKey
 
-shareScript=  [shamlet|
+sharescript=  [shamlet|
  <script>
-  function(doc, script) {
-   var js,
-       fjs = doc.getElementsByTagName(script)[0],
-       frag = doc.createDocumentFragment(),
-       add = function(url, id) {
-          if (doc.getElementById(id)) {return;}
-          js = doc.createElement(script);
-          js.src = url;
-          id && (js.id = id);
-          frag.appendChild( js );
-      };
+   function(doc, script) {
+       var js,
+           fjs = doc.getElementsByTagName(script)[0],
+           frag = doc.createDocumentFragment(),
+           add = function(url, id) {
+              if (doc.getElementById(id)) {return;}
+              js = doc.createElement(script);
+              js.src = url;
+              id && (js.id = id);
+              frag.appendChild( js );
+          };
+        // Google+ button
+        add('http://apis.google.com/js/plusone.js');
+        // Facebook SDK
+        add('//connect.facebook.net/en_US/all.js#xfbml=1&appId=200103733347528', 'facebook-jssdk');
+        // Twitter SDK
+        add('//platform.twitter.com/widgets.js');
+    
+        fjs.parentNode.insertBefore(frag, fjs);
+     }(document, 'script'));
 
-    // Google+ button
-    add('http://apis.google.com/js/plusone.js');
-    // Facebook SDK
-    add('//connect.facebook.net/en_US/all.js#xfbml=1&appId=200103733347528', 'facebook-jssdk');
-    // Twitter SDK
-    add('//platform.twitter.com/widgets.js');
+ <a href="https://twitter.com/share" class="twitter-share-button" data-count="horizontal">Tweet</a>
 
-    fjs.parentNode.insertBefore(frag, fjs);
- }(document, 'script'));
+ <div class="g-plusone" data-size="medium" data-count="true"></div>
 
-|]
-
-shareHtml= [shamlet|<a href="https://twitter.com/share" class="twitter-share-button" data-count="horizontal">Tweet</a>
-
-<div class="g-plusone" data-size="medium" data-count="true"></div>
-
-<div id="fb-root"></div>
-<div class="fb-like" data-send="false" data-layout="button_count" data-width="1" data-show-faces="false" data-action="recommend"></div>
+ <div id="fb-root"></div>
+ <div class="fb-like" data-send="false" data-layout="button_count" data-width="1" data-show-faces="false" data-action="recommend"></div>
 |]
